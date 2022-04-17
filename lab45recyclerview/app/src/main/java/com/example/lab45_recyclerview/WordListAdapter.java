9ab7f8e7c760b5d25b01926a24aa582f70dce756
vinhvn <vungocvinh95@gmail.com>
2022-04-17 15:35:54 +0700

    add 4.5

---
+++ lab45recyclerview/app/src/main/java/com/example/lab45_recyclerview/WordListAdapter.java
@@ -0,0 +1,52 @@
+package com.example.lab45_recyclerview;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.LinkedList;
+
+public class WordListAdapter extends
+        RecyclerView.Adapter<WordListAdapter.WordViewHolder>  {
+
+    private final LinkedList<String> mWordList;
+    private LayoutInflater mInflater;
+
+    public WordListAdapter(Context context,
+                           LinkedList<String> wordList) {
+        mInflater = LayoutInflater.from(context);
+        this.mWordList = wordList;
+    }
+
+    @NonNull
+    @Override
+    public WordViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull WordViewHolder holder, int position) {
+        String mCurrent = mWordList.get(position);
+        holder.wordItemView.setText(mCurrent);
+    }
+
+    @Override
+    public int getItemCount() {
+            return mWordList.size();
+        }
+
+    static class WordViewHolder extends RecyclerView.ViewHolder {
+        public final TextView wordItemView;
+        final WordListAdapter mAdapter;
+        public WordViewHolder(@NonNull View itemView, WordListAdapter adapter) {
+            super(itemView);
+            wordItemView = itemView.findViewById(R.id.word);
+            this.mAdapter = adapter;
+        }
+    }
+}
